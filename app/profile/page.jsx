54bf7c1c84b83759624ddc8ee2f81e5b47864769
QDianna <elena.diana0411@gmail.com>
2025-01-13 15:42:56 +0200

    am adaugat fetch din baza de date pentru datele utilizatorului din profile information

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,19 +2,17 @@
 
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation"; // Pentru navigare
-
 import { auth } from "../firebase/config"; // Configurarea Firebase
 import { onAuthStateChanged, signOut } from "firebase/auth"; // Funcția de logout
-// import { doc, getDoc, setDoc } from "firebase/firestore"; // Funcții pentru Firestore
-import { getDatabase, ref, set } from "firebase/database";
-
-import "../../styles/profile.css"
+import { getDatabase, ref, set, get } from "firebase/database"; // Funcții pentru citire/scriere
+import "../../styles/profile.css";
 
 const ProfilePage = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(""); // Mesajul de succes
+  const [successMessage, setSuccessMessage] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
   const [profileData, setProfileData] = useState({
     firstName: "",
     lastName: "",
@@ -26,14 +24,10 @@ const ProfilePage = () => {
   const db = getDatabase(); // Inițializăm Firebase Realtime Database
 
   useEffect(() => {
-    // Ascultă modificările stării de autentificare
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
         setUser(currentUser);
-        setProfileData((prevData) => ({
-          ...prevData,
-          email: currentUser.email,
-        }));
+        fetchProfileData(currentUser.uid); // Preia datele profilului din Firebase
       } else {
         setError("No user is signed in.");
         router.push("/auth"); // Redirecționează dacă utilizatorul nu este autentificat
@@ -44,10 +38,30 @@ const ProfilePage = () => {
     return () => unsubscribe();
   }, [router]);
 
+  // Funcție pentru preluarea datelor profilului
+  const fetchProfileData = async (uid) => {
+    try {
+      const userProfileRef = ref(db, `user_profiles/${uid}`);
+      const snapshot = await get(userProfileRef);
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        setProfileData({
+          firstName: data.firstName || "",
+          lastName: data.lastName || "",
+          phone: data.phone || "",
+          email: data.email || "",
+        });
+      }
+    } catch (err) {
+      console.error("Error fetching profile data:", err);
+      setError("Failed to load profile data.");
+    }
+  };
+
   const handleLogout = async () => {
     try {
-      await signOut(auth); // Deconectează utilizatorul
-      router.push("/auth"); // Redirecționează la pagina de autentificare
+      await signOut(auth);
+      router.push("/auth");
     } catch (err) {
       console.error("Error logging out:", err);
       setError("Failed to log out.");
@@ -60,34 +74,31 @@ const ProfilePage = () => {
   };
 
   const handleSave = async () => {
-    // Verifică dacă toate câmpurile necesare sunt completate
     if (!profileData.firstName || !profileData.lastName || !profileData.phone) {
-      setError(null); // Afișează mesajul de eroare
-      setSuccessMessage("Your profile cannot be updated!"); // Resetăm mesajul de succes
-      // Setează mesajul de succes să dispară după 3 secunde
+      setError("Please complete all fields!");
+      setSuccessMessage("");
       setTimeout(() => {
-        setSuccessMessage("");
+        setError("");
       }, 3000);
     } else {
       try {
-        const userProfileRef = ref(db, 'user_profiles/' + user.uid);
+        const userProfileRef = ref(db, "user_profiles/" + user.uid);
         await set(userProfileRef, {
           email: user.email,
           firstName: profileData.firstName,
           lastName: profileData.lastName,
           phone: profileData.phone,
         });
-        setSuccessMessage("Your profile has been updated!"); // Mesaj de succes
-        setError(null); // Resetăm mesajul de eroare
-  
-        // Setează mesajul de succes să dispară după 3 secunde
+        setSuccessMessage("Your profile has been updated!");
+        setError(""); 
         setTimeout(() => {
           setSuccessMessage("");
-        }, 3000);
+          setIsEditing(false); 
+        }, 2000);
       } catch (err) {
-          console.error("Error saving profile:", err);
-          setError("Failed to update profile. Please try again.");
-          setSuccessMessage(""); // Resetăm mesajul de succes dacă apare eroare
+        console.error("Error saving profile:", err);
+        setError("Failed to update profile. Please try again.");
+        setSuccessMessage("");
       }
     }
   };
@@ -96,14 +107,12 @@ const ProfilePage = () => {
     return <p>Loading...</p>;
   }
 
-  if (error) {
+  if (error && !isEditing) {
     return <p>{error}</p>;
   }
 
   return (
     <div className="container">
-      {/* Mesajul de succes afisat sus */}
-      {successMessage && <div className="toast-message">{successMessage}</div>}
       <div className="profile-picture">
         <img
           src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
@@ -114,43 +123,63 @@ const ProfilePage = () => {
       </div>
 
       <div className="profile-settings">
-        <h4>Profile Settings</h4>
-        <div className="input-group">
-          <input
-            type="text"
-            name="firstName"
-            placeholder="First Name"
-            value={profileData.firstName}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Last Name"
-            value={profileData.lastName}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="phone"
-            placeholder="Mobile Number"
-            value={profileData.phone}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            value={profileData.email || user.email}
-            readOnly
-          />
-          <button className="save-button" onClick={handleSave}>
-            Save Profile
-          </button>
-          <button className="view-routes-button" /*onClick={handleRoutes}*/>
-            View Saved Routes
-          </button>
-        </div>
+        <h4>Profile Information</h4>
+        {isEditing ? (
+          <div className="input-group">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={profileData.firstName}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              value={profileData.lastName}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Mobile Number"
+              value={profileData.phone}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={profileData.email || user.email}
+              readOnly
+            />
+            <button className="save-button" onClick={handleSave}>
+              Save Profile
+            </button>
+            {successMessage && <p className="success-message">{successMessage}</p>}
+            {error && <p className="error-message">{error}</p>}
+          </div>
+        ) : (
+          <div className="display-group">
+            <p>
+              <strong>First Name:</strong> {profileData.firstName || "Set first name"}
+            </p>
+            <p>
+              <strong>Last Name:</strong> {profileData.lastName || "Set last name"}
+            </p>
+            <p>
+              <strong>Mobile Number:</strong> {profileData.phone || "Set phone number"}
+            </p>
+            <p>
+              <strong>Email:</strong> {profileData.email}
+            </p>
+            <button className="edit-button" onClick={() => setIsEditing(true)}>
+              Change Profile Settings
+            </button>
+            <button className="view-routes-button">View Saved Routes</button>
+          </div>
+        )}
       </div>
 
       <button className="logout-button" onClick={handleLogout}>
@@ -161,3 +190,4 @@ const ProfilePage = () => {
 };
 
 export default ProfilePage;
+
